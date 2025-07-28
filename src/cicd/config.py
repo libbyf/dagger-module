ab9047ae7568c1276377a813df2431482a6eaba4
libbyf <libby.frenkel@skai.io>
2025-07-28 23:47:45 +0300

    Add build and test commands

--- src/cicd/config.py
+++ src/cicd/config.py
@@ -1,5 +1,5 @@
 import dagger
-from dagger import dag, function, object_type, field, Doc
+from dagger import field, Doc, object_type
 from typing import Annotated
 
 @object_type
@@ -7,4 +7,4 @@ class BaseConfig:
     component_type: Annotated[str, Doc("OpsLevel component type")] = field()
     language: Annotated[str, Doc("Project language")] = field()
     version: Annotated[str, Doc("Project Version")] = field()
-    source =  Annotated[dagger.Directory, Doc("Source point to run the commands")] = field(default=dagger.Directory("."))
+    source: Annotated[dagger.Directory, Doc("Source point to run the build")] = field(default=lambda: dagger.Directory("."))
